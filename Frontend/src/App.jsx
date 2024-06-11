0986a3e93fb16564499aabc90982e374bb47b2a3
prathambhardwaj0307 <bpratham551@gmail.com>
2024-06-11 20:49:40 +0530

    last commit of day

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Home from "./home/Home";
-import Course from "./components/Course";
 import { Route, Routes } from "react-router-dom";
+import Courses from "./courses/Courses";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/course" element={<Course />} />
+        <Route path="/course" element={<Courses />} />
       </Routes>
     </>
   );
