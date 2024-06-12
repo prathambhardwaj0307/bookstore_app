12c5d28871facf3bbc43138b5404e1a99edb8086
prathambhardwaj0307 <bpratham551@gmail.com>
2024-06-12 21:39:19 +0530

    finished courses section

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -4,7 +4,7 @@ function Cards({ item }) {
   return (
     <>
       <div className="mt-10 my-3 p-3">
-        <div className="card w-92 bg-base-100 shadow-xl">
+        <div className="card w-92 bg-base-100 shadow-xl hover:scale-105 duration-200">
           <figure>
             <img src={item.image} alt="Shoes" />
           </figure>
--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -1,24 +1,36 @@
 import React from "react";
+import Cards from "./Cards";
+import list from "../../public/list.json";
+import {Link} from "react-router-dom";
 
 function Course() {
   return (
     <>
-      <div className="fixed top-0 left-0 right-0 z-50">
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
         <div className="mt-28 items-center justify-center text-center">
-          <h1 className="text-2xl md:text-3xl">
+          <h1 className="text-2xl  md:text-4xl">
             We're delighted to have you{" "}
-            <span className="text-pink-500">Here! </span>
+            <span className="text-pink-500"> Here! :)</span>
           </h1>
           <p className="mt-12">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
+            assumenda? Repellendus, iste corrupti? Tempore laudantium
+            repellendus accusamus accusantium sed architecto odio, nisi expedita
+            quas quidem nesciunt debitis dolore non aspernatur praesentium
+            assumenda sint quibusdam, perspiciatis, explicabo sequi fugiat amet
+            animi eos aut. Nobis quisquam reiciendis sunt quis sed magnam
+            consequatur!
           </p>
-          <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">Back</button>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {list.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
         </div>
       </div>
     </>
