231b9d53ca4f14da8e259af997748f4c07e14547
prathambhardwaj0307 <bpratham551@gmail.com>
2024-06-12 20:43:23 +0530

    course page started

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Course() {
-  return <div></div>;
+  return (
+    <>
+      <div className="fixed top-0 left-0 right-0 z-50">
+        <div>
+          <h1 className="text-2xl font-semibold md:text-4xl">
+            We're delighted to have you
+            <span className="text-pink-500">Here! :) </span>
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Course;
