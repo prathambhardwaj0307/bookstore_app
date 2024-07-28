3c59bf0a82dc0fb7bac2f4a61a17755f629f3f51
prathambhardwaj0307 <bpratham551@gmail.com>
2024-07-28 23:44:03 +0530

    connection established and fetch data from mongo db

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
+import bookRoute from "./route/book.route.js";
 const app = express()
+
+app.use(cors());
 
 dotenv.config();
 
@@ -18,6 +22,9 @@ try{
 }catch(error){
   console.log("Error: ", error);
 }
+
+//defining routes
+app.use("/book", bookRoute);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
--- Frontend/src/components/FreeBook.jsx
+++ Frontend/src/components/FreeBook.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import list from "../../public/list.json";
+import axios from "axios";
 import Cards from "./Cards";
+
 function FreeBook() {
-  const filterData = list.filter((data) => data.category === "Free");
+  const[book,setBook]=useState([])
+  useEffect(() =>{
+    const getBook=async()=>{
+      try{
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        const data= res.data.filter((data) => data.category === "Free");
+        console.log(data);
+        setBook(data);
+      }catch (error) {
+        console.log("error: ",error)
+      }
+    }
+    getBook();
+  },[])
   var settings = {
     dots: true,
     infinite: false,
@@ -55,7 +70,7 @@ function FreeBook() {
 
         <div>
           <Slider {...settings}>
-            {filterData.map((item)=>(
+            {book.map((item)=>(
               <Cards item={item} key={item.id}/>
             ))}
           </Slider>
