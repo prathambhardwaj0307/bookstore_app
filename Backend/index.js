32ca06ff8a6f1a1ec553cc9b21f799fc380e436c
prathambhardwaj0307 <bpratham551@gmail.com>
2024-07-28 22:26:42 +0530

    mongo connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express()
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
 
-app.get('/', (req, res) => {
-  res.send('Test Code Working Fine!')
-})
+//connect to mongo db
+try{
+  mongoose.connect(URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  console.log("Connected to mongo db");
+}catch(error){
+  console.log("Error: ", error);
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
